96e4489e86de81e0310f99086e1903272d3474eb
DeMarcus Kirby <tkd024@gmail.com>
2020-02-14 10:59:10 -0700

    add order_action and avg_age methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def order_actors
+    actors.order(:age)
+  end
+
+  def avg_age
+    actors.average(:age)
+  end
 end

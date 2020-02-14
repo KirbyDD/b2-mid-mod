74363774b06ff936cb22bbb224fb2e3bca99a8bc
DeMarcus Kirby <tkd024@gmail.com>
2020-02-14 11:19:38 -0700

    Add update action to Movie Controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,13 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    movie = Movie.find(params[:id])
+    actor = Actor.find_by(name: params[:name])
+    if actor
+      movie.actors << actor
+      redirect_to "/movies/#{params[:id]}"
+    end
+  end
 end

71ae13ece188492211363bd3295a3f75864ce41a
DeMarcus Kirby <tkd024@gmail.com>
2020-02-14 11:18:50 -0700

    Add test for adding an actor to a movie

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -7,14 +7,24 @@ RSpec.describe "As a visitor to Actors Show Page", type: :feature do
 
     @actor1 = @movie.actors.create!(name: "Mike", age: 25)
     @actor2 = @movie.actors.create!(name: "Meg", age: 23)
+    @actor3 = Actor.create!(name: "Pam", age: 24)
+    visit "/movies/#{@movie.id}"
   end
   it 'should show studio name and all movies' do
-    visit "/movies/#{@movie.id}"
-
     expect(page).to have_content(@movie.name)
     expect(page).to have_content(@actor2.name)
     expect(page).to have_content(@actor1.name)
     expect(page).to have_content("Average Actor Age: 24")
+    expect(page).to have_button("Add")
+  end
 
+  it 'should add actors if they exist through form' do
+    expect(page).to_not have_content(@actor3.name)
+
+    fill_in :name, with: @actor3.name
+    click_on "Add"
+
+    expect(current_path).to eq("/movies/#{@movie.id}")
+    expect(page).to have_content(@actor3.name)
   end
 end

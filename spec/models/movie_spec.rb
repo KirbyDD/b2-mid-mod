6812ebce6f2bc0dd314051f0695bc09874b231cd
DeMarcus Kirby <tkd024@gmail.com>
2020-02-14 10:57:49 -0700

    Add test for methods order_actors and avg_age

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe Movie, type: :model do
     it {should have_many :movie_actors}
     it {should have_many(:actors).through(:movie_actors)}
   end
+
+  describe 'methods' do
+    it 'should order actors from youngest to oldest order_actors' do
+      studio = Studio.create!(name: "Tron Studios")
+      movie = studio.movies.create!(name: "Space Biking", creation_year: "1990", genre: "Action")
+
+      actor1 = movie.actors.create!(name: "Mike", age: 25)
+      actor2 = movie.actors.create!(name: "Meg", age: 23)
+
+      expect(movie.order_actors.first).to eq(actor2)
+      expect(movie.order_actors.last).to eq(actor1)
+
+    end
+
+    it 'should give average age of actors avg_age' do
+      studio = Studio.create!(name: "Tron Studios")
+      movie = studio.movies.create!(name: "Space Biking", creation_year: "1990", genre: "Action")
+
+      actor1 = movie.actors.create!(name: "Mike", age: 25)
+      actor2 = movie.actors.create!(name: "Meg", age: 23)
+
+      expect(movie.avg_age).to eq(24)
+    end
+  end
 end
